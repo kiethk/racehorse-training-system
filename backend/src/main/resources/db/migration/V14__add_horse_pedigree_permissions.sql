a80270d2677997823a5388d16c7647261253aa71
keith1101 <anhhuybui2006@gmail.com>
2026-09-17 19:38:30 +0700

    fix: permission UPDATE -> MANAGE

--- backend/src/main/resources/db/migration/V14__add_horse_pedigree_permissions.sql
+++ backend/src/main/resources/db/migration/V14__add_horse_pedigree_permissions.sql
@@ -1,18 +1,15 @@
--- Permissions for HorsePedigreeController
 INSERT INTO permissions (code, description) VALUES
                                                 ('HORSE_PEDIGREE_VIEW', 'View horse pedigree information'),
-                                                ('HORSE_PEDIGREE_UPDATE', 'Create or update horse pedigree information');
+                                                ('HORSE_PEDIGREE_MANAGE', 'Create or update horse pedigree information');
 
--- Grant HORSE_PEDIGREE_VIEW to HORSE_OWNER
 INSERT INTO role_permissions (role_id, permission_id)
 SELECT r.id, p.id
 FROM roles r, permissions p
 WHERE p.code = 'HORSE_PEDIGREE_VIEW'
   AND r.name = 'HORSE_OWNER';
 
--- Grant HORSE_PEDIGREE_UPDATE to HORSE_OWNER
 INSERT INTO role_permissions (role_id, permission_id)
 SELECT r.id, p.id
 FROM roles r, permissions p
-WHERE p.code = 'HORSE_PEDIGREE_UPDATE'
+WHERE p.code = 'HORSE_PEDIGREE_MANAGE'
   AND r.name = 'HORSE_OWNER';
